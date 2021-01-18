1c2c88ce33551d6a5eab23cab9b398403706a6de
First Seeds <77170748+AlunoLuisAguiar@users.noreply.github.com>
2021-01-18 18:03:51 -0200

    Create PHP Inicial2.php

---
+++ PHP Inicial2.php
@@ -0,0 +1,10 @@
+<?php
+  echo "Comando echo é como print py ou printf C";
+  // Barra barra p comentários.
+
+  echo "Inicio e fechamento super simples\n";
+  # ou hash
+
+  echo "Barra n serve para pular linha";
+  /* Ou ainda barra asterisco */
+?>
